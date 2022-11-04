b81dcd072f35a6db0d91d584c1ec7d59c57a7e29
ElizaPopa <oprea.eliza7@gmail.com>
2022-11-04 16:16:14 +0200

    new branch added with SeleniumGrid

--- src/main/java/implementation/BrowserManager.java
+++ src/main/java/implementation/BrowserManager.java
@@ -2,11 +2,24 @@ package implementation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BrowserManager {
 
+    public static RemoteWebDriver getRemoteWebDriver() throws MalformedURLException {
+        return new RemoteWebDriver(new URL("http://localhost:4444/"), new ChromeOptions());
+    }
     public static ChromeDriver getChromedriver() {
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
+    }
+    public static ChromeDriver getChromedriverWithOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(chromeOptions);
     }
 }
